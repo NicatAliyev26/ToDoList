49b8785b038656dbb0646c1f30308423d2fee908
Nicat Aliyev <67416106+NicatAliyev26@users.noreply.github.com>
2021-03-09 20:12:35 +0400

    Update app.js
    add style cursor

--- app.js
+++ app.js
@@ -14,4 +14,17 @@ $('ul').on('click', "span" , function(event){
   });
   event.stopPropagation();
 });
+
+
+
+// style cursor
+var place = document.getElementsByTagName("p")[0];
+place.style.position = "absolute";
+place.style.color = "red";
+place.style.fontSize = "25px";
+
+window.onmousemove = function(event){
+    place.style.left = event.clientX + 'px';
+    place.style.top = event.clientY + 'px';
+}
     
